d8ac08a0ee10ba6b71a0da131cc2f0e85cac0062
Francisco-93 <francisco.fro.agui@gmail.com>
2020-07-07 17:45:43 -0300

    ServicosDepartamento

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -27,13 +34,16 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obslist;
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
 	
-	
-	
+	public void setDepartmentService (DepartmentService service) {
+		this.service = service;
+	}
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -41,11 +51,24 @@ public class DepartmentListController implements Initializable {
 	}
 
 	private void initializeNodes() {
-		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id"));
-		tablecolumnName.setCellValueFactory(new PropertyValueFactory<>("Nome"));
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tablecolumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 		
 		//Para ajustar a tabela na tela
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obslist = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obslist);
+	}
+	
+	
+	
+	
 }
